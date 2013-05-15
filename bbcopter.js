5511b5a1865e177a79d8c10eccd637b250764761
Timothe Fillion Brunet <timothefilb@gmail.com>
2013-05-15 13:39:56 -0400

    fix package json to download bbcopterServer from git until we add it into npmjs.org db

--- bbcopter.js
+++ bbcopter.js
@@ -1,17 +1,16 @@
+//load node module requirements
 var fs = global.fs = require('fs');
 var util = require("util");
 
+//load configuration file
 var config = global.config = JSON.parse(fs.readFileSync('./config/config.json'));
 
-//global const information
+//load global variables informations
 var gl = global.gl = new require("./config/global.js")(config);
-
-//load module
-console.log(gl);
 
 /**
  * We load sensors here depending the config.json.
- * if Degree of freedom (DOF) is 6 we will not load the mag module, if DOF is 9 it will include the mag.  
+ * if Degree of freedom (DOF) is 6 we will not load the mag module, if DOF is 9 it will include the mag.
  */
 try {
 	var accel = global.accel = require(config.sensors.accel);
@@ -35,3 +34,10 @@ if (global.DOF == 9) {
 	}
 }
 
+// We load motor module
+try {
+	var motor = global.motor = require(config.motor);
+} catch(err) {
+	console.log("Cannot load mag module!\nPlease check your config.json file and you need to have the same name for your module under node_module folder");
+	global.motorOK = false;
+}
