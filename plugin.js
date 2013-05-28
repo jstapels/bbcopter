7cbe94b6aefb440491b47facbf7b4b957c87ead8
jstapels <jstapels@gmail.com>
2013-05-28 10:57:53 -0400

    Removed bogus packets and pins class. Plugins just need to document the types of packets they provide, and the pin "sharing" needs to be fleshed out some more.
    Updates packet class to be a little bit more OOPy.

--- plugin.js
+++ plugin.js
@@ -2,7 +2,7 @@
  * @module plugins
  */
 var util = require('util');
-var events = require('events');
+var EventEmmiter = require('events').EventEmmiter;
 var packet = require('./packet');
 
 
@@ -10,8 +10,16 @@ var packet = require('./packet');
  * The plugin manager. All plugins must register with this manager.
  */
 function PluginManager() {
-    
+	this.plugins = [];
 }
+
+PluginManager.prototype.registerPlugin = function(plugin) {
+	
+};
+
+PluginManager.prototype.unregisterPlugin = function(plugin) {
+	
+};
 
 
 
@@ -26,42 +34,64 @@ function PluginManager() {
  * @this {Plugin}
  */
 function Plugin() {
-	events.EventEmitter.call(this);
-    
-    this.name = "Abstract Plugin";
-    this.description = "An abstract description";
-    this.major = 0;
-    this.minor = 0;
-    this.requirePackets = [];
-    this.optionalPackets = [];
-    this.requiredPins = [];
-    this.optionalPins = [];
-    this.providedPackets = [];
-    
-    this._packets = {};
+  EventEmitter.call(this);
+  
+  // Private methods/functions
+  var packetData = {};
+  
+  // Privileged methods
+  this.getPacket = function(type) {
+    return packetData[type];
+  };
+
+  this.setPacket = function(type, value) {
+    var old = packetData[type];
+    packetData[type] = value;
+
+    if (old != value) {
+      this.emit(type, value);
+    }
+  };
+
+  // Public variables
+  this.name = "Abstract Plugin";
+  this.description = "An abstract description";
+  this.majorVersion = 0;
+  this.minorVersion = 0;
+  this.requirePackets = [];
+  this.optionalPackets = [];
+  this.providedPackets = [];
 }
 
-util.inherits(Plugin, events.EventEmitter);
+// Extend the EventEmmiter class.
+Plugin.prototype = Object.create(EventEmmiter.prototype);
+Plugin.prototype.constructor = Plugin;
 
-Plugin.prototype.activate = function(plugins) {
-    console.log("Plugin: " + this.name + " activated");
+// Public methods
+
+/**
+ * Called when the plugin is first initialized.
+ * <p>
+ * Note that this plugin doesn't have access to other plugins at this time.
+ * All referenced to dependant plugins need to happen during the activattion.
+ */
+Plugin.prototype.initialize = function() {
+  
 };
 
-Plugin.prototype.deactivate = function(plugins) {
-    console.log("Plugin: " + this.name + " deactivated");
+/**
+ * Called when your plugin is activated.
+ */
+Plugin.prototype.activate = function() {
+  console.log("Plugin: " + this.name + " activated");
 };
 
-Plugin.prototype.getPacket = function(type) {
-	return this._packets[type];
-};
-
-Plugin.prototype.setPacket = function(type, value) {
-    var old = this._packets[type];
-	this._packets[type] = value;
-        
-    if (old != value) {
-        this.emit(type, value);
-    }
+/**
+ * Called when your plugin is deactivates.
+ * @param plugins
+ */
+Plugin.prototype.deactivate = function() {
+  console.log("Plugin: " + this.name + " deactivated");
 };
 
 // Module exports.
